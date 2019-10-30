b68ac42ee4194603d49aefaed7d4764303b10802
Alex Shapiro <ags245@cornell.edu>
2019-10-30 17:20:02 -0400

    it

--- Project/game.ml
+++ Project/game.ml
@@ -1,13 +1,48 @@
 
 (* unbounded forward back, left right, diag, or finite number steps *)
-type move = FB | LR
+(*type move = FB | LR | D | Step of (int*int)*)
+type move_t = U | F of int | FB of int
+type move = C | R (* column or row of either unbounded, positive,
+                     or positive negative*)
 
-type chess_names = Knight | Pawn
+type piece_names =  Knight | Pawn | Bishop | Rook | Queen | King
 
-type piece = {
-  name : chess_names;
+type piece_type = {
+  piece : piece_names;
   points : int;
-  val_move : move;
-  loc : int*int;
 }
 
+type piece = {
+  p : piece_type;
+  loc : int*int;
+  alive : bool;
+  first_move : bool;
+}
+
+type t = {
+  piece_types : piece_type list;
+  start_pieces : piece list;
+  board_size : int;
+}
+
+let pawn = {
+  name = Pawn;
+  points = 3;
+  moves1 = [(1,0); (2,0)];
+  moves2 = [(1,0)];
+  first_move = true;
+}
+
+let pawn = {
+  
+}
+
+let moves p = 
+  match p with
+  | {Pawn,loc,alive,first_move} -> 
+    if first_move then
+      [(]
+
+
+let init_from_json json = {
+}
--- Project/game.mli
+++ Project/game.mli
@@ -8,7 +8,7 @@
 (** abstract type representing pieces of the game *)
 type piece
 
-(** abstract type of value representing game *)
+(** abstract type of value representing game setup *)
 type t
 
 (** type representing a move in the game *)
@@ -17,6 +17,8 @@ type move
 (** [moves p g] is the set-like list of moves of piece [p] in game [g]. *)
 val moves : piece -> t -> move list
 
-(** [from_json j] is the game with initial piece placement that [j]
+val piece_names : t -> piece list
+
+(** [init_from_json j] is the game with initial piece placement that [j]
     represents. *)
-val from_json : Yojson.Basic.t -> t
+val init_from_json : Yojson.Basic.t -> t
