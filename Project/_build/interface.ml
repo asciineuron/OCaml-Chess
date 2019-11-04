6318ff4fdd014b0981e541b73ec2524d1f1f8734
Alex Shapiro <ags245@cornell.edu>
2019-11-04 16:02:10 -0500

    ok

--- Project/_build/interface.ml
+++ Project/_build/interface.ml
@@ -1,31 +1,4 @@
 open State
-<<<<<<< HEAD
-let print_piece piece =
-  match piece with 
-  |None -> print_endline "       |"
-             print_endline "       |"
-             print_endline "       |"
-  |Some(Pawn) -> print_endline "    _   |"
-                   print_endline "  ( )  |"
-                   print_endline "  /_\  |"
-  |Some(Knight) -> print_endline "  %~\  |"
-                     print_endline " `')(  |"
-                     print_endline "  <__> |"
-  |Some(Bishop) -> print_endline "  .O.  |"
-                     print_endline "  \ /  |"
-                     print_endline "  /_\  |"
-  |Some(Rook) -> print_endline " [___] |"
-                   print_endline "  [ ]  |"
-                   print_endline " /___\ |"
-  |Some(Queen) -> print_endline " \o^o/ |"
-                    print_endline "  [ ]  |"
-                    print_endline " /___\ |"
-  |Some(King) -> print_endline " __+__ |"
-                   print_endline " `. .' |"
-                   print_endline " /___\ |"
-
-let print_board lst =
-=======
 
 let print_piece (piece : State.piece option) =
   match piece with 
@@ -55,7 +28,6 @@ let print_piece (piece : State.piece option) =
     end
 
 let print_board game =
->>>>>>> 86d0d04e925b1d5d0e2981491823b0e059f2093f
   print_endline  "   A       B       C       D       E       F       G       H   ";
   for i=0 to 7 do
     print_string " |";
@@ -64,12 +36,7 @@ let print_board game =
     for j=0 to 7 do
       let pos = (i,j) in 
       print_string " -------";
-<<<<<<< HEAD
-      print_piece (State.get_piece pos State.t)
-        print_string " -------";
-=======
       print_piece (State.get_piece pos game);
       print_string " -------";
->>>>>>> 86d0d04e925b1d5d0e2981491823b0e059f2093f
     done
   done
